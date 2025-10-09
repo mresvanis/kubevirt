983a6f3e08459e1650888ed1862e77c658f391c6
Michail Resvanis <mresvani@redhat.com>
2025-10-09 15:21:22 +0200

    Add PCIe controller fields to virtwrap API schema
    Signed-off-by: Michail Resvanis <mresvani@redhat.com>

--- pkg/virt-launcher/virtwrap/api/schema.go
+++ pkg/virt-launcher/virtwrap/api/schema.go
@@ -101,6 +101,13 @@ const (
 	HostDeviceUSB  = "usb"
 	AddressPCI     = "pci"
 	AddressCCW     = "ccw"
+
+	ControllerTypePCI                   = "pci"
+	ControllerModelPCIeRoot             = "pcie-root"
+	ControllerModelPCIeRootPort         = "pcie-root-port"
+	ControllerModelPCIeExpanderBus      = "pcie-expander-bus"
+	ControllerModelPCIeSwitchUpstream   = "pcie-switch-upstream-port"
+	ControllerModelPCIeSwitchDownstream = "pcie-switch-downstream-port"
 )
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -656,6 +663,7 @@ type Controller struct {
 	Alias     *Alias            `xml:"alias,omitempty"`
 	Address   *Address          `xml:"address,omitempty"`
 	PCIHole64 *PCIHole64        `xml:"pcihole64,omitempty"`
+	Target    *ControllerTarget `xml:"target,omitempty"`
 }
 
 // END Controller -----------------------------
@@ -676,6 +684,14 @@ type PCIHole64 struct {
 }
 
 // END PCIHole64
+
+// BEGIN ControllerTarget
+type ControllerTarget struct {
+	BusNr *uint32 `xml:"busNr,attr,omitempty"`
+	Node  *uint32 `xml:"node,omitempty"`
+}
+
+// END ControllerTarget
 
 // BEGIN Disk -----------------------------
 
